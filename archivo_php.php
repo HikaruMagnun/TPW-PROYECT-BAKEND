dbd3efff066cafd62f93de2bb1ae8d4357afdd57
HikaruMagnun <carlosdiazramos25@hotmail.com>
2023-06-05 15:12:29 -0500

    witch postman

--- archivo_php.php
+++ archivo_php.php
@@ -1,23 +1,25 @@
 <?php
-$apiUrl = 'https://fnaxymkdzcvseznghoup.supabase.co/rest/v1/Apple?select=*';
-$apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZuYXh5bWtkemN2c2V6bmdob3VwIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4NDU1NDgzMywiZXhwIjoyMDAwMTMwODMzfQ.ENla18HCCgESdG3Ks081apJoPOccmH4iVQcmaaHCa5Q'; // Reemplaza con tu propia clave API de Supabase
-
-$options = array(
-    'http' => array(
-        'header' => "apikey: $apiKey"
-    )
-);
-$context = stream_context_create($options);
-$response = file_get_contents($apiUrl, false, $context);
-
-if ($response) {
-    $data = json_decode($response, true);
-    // Imprimir el resultado
-    print_r($data);
-} if (curl_errno($ch)) {
-    echo 'Error en la solicitud: ' . curl_error($ch);
-}
-else {
-    echo "Error al realizar la solicitud a la API de Supabase.";
-}
+curl_setopt_array($curl, [
+    CURLOPT_URL => "https://fnaxymkdzcvseznghoup.supabase.co/rest/v1/Apple?select=*",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "GET",
+    CURLOPT_HTTPHEADER => [
+      "apikey: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZuYXh5bWtkemN2c2V6bmdob3VwIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4NDU1NDgzMywiZXhwIjoyMDAwMTMwODMzfQ.ENla18HCCgESdG3Ks081apJoPOccmH4iVQcmaaHCa5Q"
+    ],
+  ]);
+  
+  $response = curl_exec($curl);
+  $err = curl_error($curl);
+  
+  curl_close($curl);
+  
+  if ($err) {
+    echo "cURL Error #:" . $err;
+  } else {
+    echo $response;
+  }
 ?>
