36c87783d1146d42e483625f5529dd590ccb90fe
HikaruMagnun <carlosdiazramos25@hotmail.com>
2023-06-05 15:33:46 -0500

    consulta de tablas

--- archivo_php.php
+++ archivo_php.php
@@ -10,7 +10,21 @@ $dsn = "pgsql:host=$host;port=$port;dbname=$dbname;user=$user;password=$password
 try {
     $db = new PDO($dsn);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Conexión exitosa a la base de datos PostgreSQL.";
+    // Obtener las tablas disponibles
+    $query = "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'";
+    $stmt = $db->prepare($query);
+    $stmt->execute();
+    $tables = $stmt->fetchAll(PDO::FETCH_COLUMN);
+
+    // Mostrar las tablas
+    if (count($tables) > 0) {
+        echo "Tablas disponibles:<br>";
+        foreach ($tables as $table) {
+            echo "- $table<br>";
+        }
+    } else {
+        echo "No hay tablas disponibles en la base de datos.";
+    }
 } catch (PDOException $e) {
     echo "Error al conectar a la base de datos: " . $e->getMessage();
 }
