7382c6f98aa8cf1830b1885d551a83c504664443
HikaruMagnun <carlosdiazramos25@hotmail.com>
2023-06-06 08:53:33 -0500

    contacto compatibilidad

--- Contacto.php
+++ Contacto.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contacto</title>
-    <link rel="stylesheet" href="contacto.css">
+    <link rel="stylesheet" href="css/contacto.css">
     <!--Font Awesome-->
     <script src="https://kit.fontawesome.com/846a5e64df.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
