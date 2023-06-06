c0458324ac58147fc6e79ab9c46bdce02bec43e4
HikaruMagnun <carlosdiazramos25@hotmail.com>
2023-06-05 19:45:11 -0500

    slash fix

--- Apple.php
+++ Apple.php
@@ -172,7 +172,7 @@
             </table>
             </center>
         </main>
-        <?php include 'php\archivo_php.php'?>
+        <?php include 'php/archivo_php.php'?>
 
     </div>
     <script src="js/celulares.js"></script>
