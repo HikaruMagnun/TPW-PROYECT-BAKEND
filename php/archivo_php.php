823b4bbf49ce00cc0c4612d776e52ea0c9559b07
HikaruMagnun <carlosdiazramos25@hotmail.com>
2023-06-05 20:05:48 -0500

    dinamic test

--- php/archivo_php.php
+++ php/archivo_php.php
@@ -10,20 +10,35 @@
     // ID, NOMBRE, PANTALLA , BATERIA, SISTEMA OPERATIVO, CAMARA, CAPACIDAD , IMAGE(link), PRECIO 
    
     $tableRows = ''; // Variable para almacenar las filas de la tabla
+    $counter = 0;
     foreach ($result as $row) {
+        if ($counter % 4 === 0) {
+            echo '<tr>';
+        }
+    
+        echo '<td>';
+        echo '<img src="' . $row['Image'] . '" alt="Imagen del producto">';
+        echo '</td>';
+    
+        echo '<td>';
         echo '<h4>' . $row['Nombre'] . '</h4>';
         echo '<p>Pantalla: ' . $row['Pantalla (pulgadas)'] . '</p>';
         echo '<p>Batería: ' . $row['Bateria (mAh)'] . '</p>';
         echo '<p>Sistema Operativo: ' . $row['S.O.'] . '</p>';
         echo '<p>Cámara: ' . $row['Camara (MP)'] . '</p>';
         echo '<p>Capacidad: ' . $row['Capacidad (GB)'] . '</p>';
-        echo '<img src="' . $row['Image'] . '" alt="Imagen del producto">';
         echo '<p>Precio: ' . $row['Precio'] . '</p>';
-        echo '<hr>'; // Separador entre cada fila
+        echo '</td>';
+    
+        $counter++;
+        if ($counter % 4 === 0) {
+            echo '</tr>';
+        }
     }
+    if ($counter % 4 !== 0) {
+        echo '</tr>';
+    }   
     
     
-    $tableRows .= '<h1> Esto es codigo html </h1>';
-    echo $tableRows;
 
 ?>
