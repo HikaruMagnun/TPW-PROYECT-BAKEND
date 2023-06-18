5ee9cf1ce98bbd05119c05e5d1ce3981b3124cdc
HikaruMagnun <carlosdiazramos25@hotmail.com>
2023-06-18 13:20:14 -0500

    login <= index

---
+++ mainPage.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script
+      src="https://kit.fontawesome.com/076516437a.js"
+      crossorigin="anonymous"
+    ></script>
+    <title>MOVILAND</title>
+    <link rel="icon" href="https://lh3.googleusercontent.com/u/0/d/1BztHznYZ5NnudZ0HIE3dOwI6VvcMVy_r=w1366-h653-iv1"/>
+    <link rel="stylesheet" href="css/css.css" />
+  </head>
+  <body>
+    <div class="page">
+      <div class="menu">
+        <div class="menu_logo">
+          <img
+            src="https://lh3.googleusercontent.com/u/0/d/1BztHznYZ5NnudZ0HIE3dOwI6VvcMVy_r=w1366-h653-iv1"
+            alt=""
+          />
+        </div>
+        <nav class="menu_pestaña">
+          <ul>
+            <li>
+              <div
+                class="menu_pestaña_elememt"
+                id="menu_pestaña_elememt_inicio"
+                data-url="Inicio.html"
+              >
+                &nbsp;
+                <i class="fa-solid fa-house"></i>
+                <h4>INICIO</h4>
+              </div>
+            </li>
+            <li>
+              <div
+                class="menu_pestaña_elememt"
+                id="menu_pestaña_elememt_categorias"
+                data-url="Samsung.php"
+              >
+                &nbsp;
+                <i class="fa-solid fa-bars"></i>
+                <h4>CATEGORIAS</h4>
+              </div>
+            </li>
+            <li>
+              <div class="menu_pestaña_elememt" data-url="Carrito.html">
+                &nbsp;
+                <i class="fa-solid fa-cart-shopping"></i>
+                <h4>CARRITO</h4>
+              </div>
+            </li>
+            <li>
+              <div class="menu_pestaña_elememt" data-url="Nosotros.html">
+                &nbsp;
+                <i class="fa-solid fa-users"></i>
+                <h4>NOSOTROS</h4>
+              </div>
+            </li>
+            <li>
+              <div class="menu_pestaña_elememt" data-url="Contacto.html">
+                &nbsp;
+                <i class="fa-solid fa-phone-volume"></i>
+                <h4>CONTACTANOS</h4>
+              </div>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div id="contenido">
+        <iframe name="main" frameborder="0"></iframe>
+      </div>
+    </div>
+    <script src="js/test.js"></script>
+  </body>
+</html>
