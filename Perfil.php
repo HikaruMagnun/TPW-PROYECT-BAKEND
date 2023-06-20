ea1098b669b5a927ac7861e0d11b3b12ad9b1893
HikaruMagnun <carlosdiazramos25@hotmail.com>
2023-06-19 19:27:10 -0500

    back

--- Perfil.php
+++ Perfil.php
@@ -9,16 +9,16 @@
 
 <body>
     <?php
-        
+    session_start(); // Iniciar la sesión
 
-        // Verificar si el correo electrónico está almacenado en la variable de sesión
-        if (isset($_SESSION['correo_usuario'])) {
-            $correo_usuario = $_SESSION['correo_usuario'];
-            // Utilizar el correo electrónico como desees
-            echo "El correo electrónico del usuario es: " . $correo_usuario;
-        } else {
-            echo "error al iniciar sesion";
-        }
+    // Verificar si el correo electrónico está almacenado en la variable de sesión
+    if (isset($_SESSION['correo_usuario'])) {
+        $correo_usuario = $_SESSION['correo_usuario'];
+        // Utilizar el correo electrónico como desees
+        echo "El correo electrónico del usuario es: " . $correo_usuario;
+    } else {
+        echo "error al iniciar sesion";
+    }
     ?>
 
 </body>
