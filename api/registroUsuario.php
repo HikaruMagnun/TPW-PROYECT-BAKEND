fcf2533cf5c95de8eb5bafdfa0152499532e0dc7
HikaruMagnun <carlosdiazramos25@hotmail.com>
2023-07-14 11:51:52 -0500

    fsd

--- api/registroUsuario.php
+++ api/registroUsuario.php
@@ -3,7 +3,7 @@
     $correo = $_POST['correo'];
     $contrasena = $_POST['contrasena'];
     //conexion
-    include 'api/conexion.php';
+    include 'api/conexion';
     //buscamos usuario
     $query = 'SELECT * FROM "USUARIOS" WHERE correo = :correo';
     $stmt = $db->prepare($query);
