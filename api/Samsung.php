623b034d18c28eee377b150d4516a3abd55c344a
HikaruMagnun <carlosdiazramos25@hotmail.com>
2023-07-16 11:20:01 -0500

    asd

--- api/Samsung.php
+++ api/Samsung.php
@@ -22,10 +22,10 @@ $correoUsuario = $_GET['correo_usuario'];
         <header>
             <nav class="celulares_nav">
                 <ul class="nav-links">
-                    <li><a onclick="cargarContenido('../api/Samsung.php?correo_usuario=<?php echo $correoUsuario?>')">SAMSUNG</a></li>
-                    <li><a onclick="cargarContenido('../api/Apple.php?correo_usuario=<?php echo $correoUsuario?>')"> APPLE</a></li>
-                    <li><a onclick="cargarContenido('../api/Huawei.php?correo_usuario=<?php echo $correoUsuario?>')">HUAWEI</a></li>
-                    <li><a onclick="cargarContenido('../api/Xiaomi.php?correo_usuario=<?php echo $correoUsuario?>')">XIAOMI</a></li>
+                    <li><a onclick="cargarContenido('../api/Samsung.php?correo_usuario=<?php echo $correoUsuario ?>')">SAMSUNG</a></li>
+                    <li><a onclick="cargarContenido('../api/Apple.php?correo_usuario=<?php echo $correoUsuario ?>')"> APPLE</a></li>
+                    <li><a onclick="cargarContenido('../api/Huawei.php?correo_usuario=<?php echo $correoUsuario ?>')">HUAWEI</a></li>
+                    <li><a onclick="cargarContenido('../api/Xiaomi.php?correo_usuario=<?php echo $correoUsuario ?>')">XIAOMI</a></li>
                 </ul>
 
 
@@ -45,30 +45,30 @@ $correoUsuario = $_GET['correo_usuario'];
     </div>
     <script src="../js/celulares.js"></script>
     <script>
-        $(document).ready(function() {
-            $('#myForm').submit(function(event) {
-                event.preventDefault(); // Prevenir el envío predeterminado del formulario
+        function enviarFormulario(id_celular, correo_usuario) {
+            // Crear un objeto con los datos del formulario
+            var formData = {
+                id_celular: id_celular,
+                correo_usuario: correo_usuario
+            };
 
-                // Obtener los datos del formulario
-                var formData = $(this).serialize();
-
-                // Enviar los datos del formulario mediante AJAX
-                $.ajax({
-                    url: '../api/añadirCarrito.php',
-                    type: 'POST',
-                    data: formData,
-                    success: function(response) {
-                        // Realizar acciones después de recibir la respuesta del servidor
-                        mostrarNotificacion();
-                    },
-                    error: function() {
-                        console.log('Error al enviar el formulario');
-                    }
-                });
+            // Enviar los datos del formulario mediante AJAX
+            $.ajax({
+                url: '../api/añadirCarrito.php',
+                type: 'POST',
+                data: formData,
+                success: function(response) {
+                    // Realizar acciones después de recibir la respuesta del servidor
+                    mostrarNotificacion();
+                },
+                error: function() {
+                    console.log('Error al enviar el formulario');
+                }
             });
-        });
+        }
 
-       
+
+
 
         function mostrarNotificacion() {
             Swal.fire({
