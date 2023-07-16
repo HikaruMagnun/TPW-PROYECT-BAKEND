a402e9e9eb602ec847dd14b8a0b5420539f0a325
HikaruMagnun <carlosdiazramos25@hotmail.com>
2023-07-16 16:19:39 -0500

    sdgdf

--- api/Tabla_Carrito.php
+++ api/Tabla_Carrito.php
@@ -2,26 +2,31 @@
 
 include __DIR__ . '/conexion.php';
 
-$querySelect = 'SELECT C."Nombre",C."Precio" ,cantidad,P.id_celular, P.id from "Pedidos" P,"Celulares" C where C."id" = P."id_celular" and P.correo =  \':correo \'';
+
+$querySelect = 'SELECT C."Nombre", C."Precio", cantidad, P.id_celular, P.id 
+                FROM "Pedidos" P, "Celulares" C 
+                WHERE C."id" = P."id_celular" AND P.correo = :correo';
+
 $stmtSelect = $db->prepare($querySelect);
-$stmtSelect->execute([':correo' => $correoUsuario]);
+$stmtSelect->bindParam(':correo', $correoUsuario);
+$stmtSelect->execute();
 
-$result = $stmtSelect->fetch(PDO::FETCH_ASSOC);
+$result = $stmtSelect->fetchAll(PDO::FETCH_ASSOC);
 
-if($result){
+if ($result) {
     foreach ($result as $row) {
-        echo '<td>' .$row ['Nombre'] .' $</td>';
-        echo '<td>'.$row ['Precio'].'</td>';
+        echo '<td>' . $row['Nombre'] . ' $</td>';
+        echo '<td>' . $row['Precio'] . '</td>';
         echo '<td>';
         echo '  <button onclick="subirCantidad(1)"> ↑ </button>';
-        echo '  <span id="cantidad-1"> '.$row['cantidad'] .' </span>';
+        echo '  <span id="cantidad-1">' . $row['cantidad'] . ' </span>';
         echo '  <button onclick="bajarCantidad(1)"> ↓ </button>';
         echo '</td>';
         echo '<td id="total-1">$100</td>';
-       echo ' <td><button onclick="eliminarFila(1)">Eliminar</button></td>';
-     }
-
+        echo ' <td><button onclick="eliminarFila(1)">Eliminar</button></td>';
+    }
 }
+
 
 
 ?>
