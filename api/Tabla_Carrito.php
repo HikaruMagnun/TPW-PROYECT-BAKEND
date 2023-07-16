612f230d7cdbb65a0a08e828946c6cf1132b3fc6
HikaruMagnun <carlosdiazramos25@hotmail.com>
2023-07-16 15:00:30 -0500

    aaaa

--- api/Tabla_Carrito.php
+++ api/Tabla_Carrito.php
@@ -1,4 +1,4 @@
-<?
+<?php
 
 include __DIR__ . '/conexion.php';
 
@@ -6,17 +6,20 @@ $querySelect = 'SELECT C."Nombre",C."Precio" ,cantidad,P.id_celular, P.id from "
 $stmtSelect = $db->prepare($querySelect);
 $stmtSelect->bindParam(':correo', $correoUsuario);
 $stmtSelect->execute();
-$row = $stmtSelect->fetch(PDO::FETCH_ASSOC);
 
-
-foreach ($result as $row) {
-   echo '<td>' .$row ['Nombre'] .' $</td>';
-   echo '<td>'.$row ['Precio'].'</td>';
+while ($row = $stmtSelect->fetch(PDO::FETCH_ASSOC)) {
+   echo '<tr>';
+   echo '<td>' .$row['Nombre'] .' $</td>';
+   echo '<td>'.$row['Precio'].'</td>';
    echo '<td>';
    echo '  <button onclick="subirCantidad(1)"> ↑ </button>';
    echo '  <span id="cantidad-1"> '.$row['cantidad'] .' </span>';
    echo '  <button onclick="bajarCantidad(1)"> ↓ </button>';
    echo '</td>';
    echo '<td id="total-1">$100</td>';
-  echo ' <td><button onclick="eliminarFila(1)">Eliminar</button></td>';
+   echo ' <td><button onclick="eliminarFila(1)">Eliminar</button></td>';
+   echo '</tr>';
 }
+
+?>
+
