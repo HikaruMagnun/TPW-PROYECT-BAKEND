e4afb47948334b025937715aae462602ed04fdda
HikaruMagnun <carlosdiazramos25@hotmail.com>
2023-07-18 03:15:26 -0500

    3:15

--- api/Tabla_switch.php
+++ api/Tabla_switch.php
@@ -4,21 +4,30 @@ $correo = $_POST['correo'];
 
 
 
-   // Iniciar una transacción
-   $db->beginTransaction();
-    // Consulta para eliminar las filas según el correo
-    $eliminarStmt = $db->prepare("DELETE FROM Pedidos WHERE correo = :correo");
-    $eliminarStmt->bindParam(':correo', $correo);
-    $eliminarStmt->execute();
 
-    // Consulta para insertar las filas eliminadas en la tabla Pedidos_Pagados
-    $insertarStmt = $db->prepare("INSERT INTO Pedidos_Pagados (id, correo, id_celular, cantidad) SELECT id, correo, id_celular, cantidad FROM Pedidos WHERE correo = :correo");
+    // Consulta para insertar las filas seleccionadas en Pedidos_Pagados
+    $insertarStmt = $db->prepare('INSERT INTO "Pedidos_Pagados" (id, correo, id_celular, cantidad)
+        SELECT id, correo, id_celular, cantidad
+        FROM "Pedidos"
+        WHERE correo = :correo');
     $insertarStmt->bindParam(':correo', $correo);
     $insertarStmt->execute();
 
-    // Confirmar la transacción
-    $db->commit();
-    header("Location:/api/Carrito.php?correo_usuario=". $correo);
+    echo "Filas insertadas en Pedidos_Pagados correctamente.";
 
-    echo "Filas eliminadas y trasladadas a Pedidos_Pagados correctamente.";
+    $eliminarStmt = $db->prepare('DELETE FROM "Pedidos" WHERE correo = :correo');
+    $eliminarStmt->bindParam(':correo', $correo);
+    $eliminarStmt->execute();
+
+    echo "Filas eliminadas correctamente.";
+    echo "<script> window.location.href = '/api/Carrito.php';</script>";
+
+
+
+
+
+
+
+echo "Filas eliminadas y trasladadas a Pedidos_Pagados correctamente.";
+
 ?>
